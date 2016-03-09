c7d55e314f22592c5e331bea481fafc6a1e2046b
Sarah Read-Brown <sreadbrown@gmail.com>
2016-03-08 17:58:40 -0800

    add the jquery for a for loop to count up to input number

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+var pingPong = function(n) {
+  var text = ""
+
+  for (var i = 1; i <= n; i++) {
+    text += i
+  }
+ return text;
+}
+
+if (n % 3 === 0) {
+  return  ping
+}
+
+if (n % 5 === 0) {
+  return pong
+}
+if (n % 5 === 0 || n % 5 === 0)
