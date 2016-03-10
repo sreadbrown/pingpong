fc263bfa98caf605dbab37059f7fd8b9a6e3f314
Sarah Read-Brown <sreadbrown@gmail.com>
2016-03-09 16:29:20 -0800

    add jquery logic for returning pong for numbers div by five and ping pong for num div by 3 and 5

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,17 @@
 var pingPong = function(n) {
   var text = "";
   for (var i = 1; i <= n; i++) {
-    if (i % 3 === 0) {
-      text += "ping";
+    if (i % 15 === 0) {
+      text += "div 15!";
+    }
+    else if (i % 5 === 0) {
+      text += "div by 5 ";
+    }
+    else if (i % 3 === 0) {
+      text += "div by 3 ";
     }
     else {
-      text += i + "<br>";
+      text += i + "<br> ";
     }
   }
   return text;
